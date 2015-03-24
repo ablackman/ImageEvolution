4ee3fedbee61386efba0c92582d9b21eaf197dc8
ablackman <ablackman@cherrycreekschools.org>
2015-03-24 08:58:22 -0600

    Pls work

--- ImageEvolution/src/EvolutionApplet.java
+++ ImageEvolution/src/EvolutionApplet.java
@@ -3,32 +3,44 @@ import java.awt.event.*;
 import java.awt.image.*;
 import java.io.IOException;
 import java.net.URL;
-
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class EvolutionApplet extends JApplet {
+    private JLabel targetTitle;
+    private JLabel bestTitle;
+    private JLabel evolvingTitle;
+	
     private JLabel targetDisplayLabel;
     private JLabel bestDisplayLabel;
     private JLabel evolvingDisplayLabel;
+    
+    private JLabel fitnessLabel;
+    private JLabel generationLabel;
+    private JLabel improvementLabel;
    
     private JButton getImageButton;
     private JButton toggleStartButton;
-    
-    private JPanel optionPanel;
-    private JSpinner mutationSpinner;
     
     private JFileChooser fc;
     private BufferedImage targetImage;
     
     private CircleIndividual bestImage;
     private CircleIndividual evolvingImage;
+    
+    private int generation = 0;
+    private int improvements = 0;
     private double bestFitness = 0;
+    
+    Font titleFont = new Font("Sans Serif", Font.BOLD, 28);
+    Font statFont = new Font("Sans Serif", Font.BOLD, 24);
     
     private boolean isStart = false;
     
-    Thread t;
+    SpringLayout spring = new SpringLayout();
+    
+    private Thread t;
     
     @Override
     public void init() {
@@ -48,62 +60,123 @@ public class EvolutionApplet extends JApplet {
             	if((targetImage.getRGB(x, y) >> 24 & 0xFF) < 0xFF) {
             		targetImage.setRGB(x,  y, 0xFFFFFFFF);
             	}
-//            	int a = 0xFF - ((targetImage.getRGB(x,  y) >> 24) & 0xFF);
-//            	int r = (targetImage.getRGB(x,  y) >> 16) & 0xFF + a;
-//            	int g = (targetImage.getRGB(x,  y) >> 8) & 0xFF + a;
-//            	int b = targetImage.getRGB(x,  y) & 0xFF + a;
-//            	targetImage.setRGB(x, y, (0xFF << 24) | (r << 16) | (g << 8) | b);
             }
         }
         
         evolvingImage = new CircleIndividual(targetImage.getWidth(), targetImage.getHeight(), 256);
         bestImage = evolvingImage;
         
-        setLayout(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
+        setLayout(spring);
+        
+        //Create target image title
+        targetTitle = new JLabel();
+        targetTitle.setText("Target");
+        targetTitle.setFont(titleFont);
+        add(targetTitle);
+        spring.putConstraint(SpringLayout.NORTH, targetTitle, 5,
+                             SpringLayout.NORTH, this);
+        spring.putConstraint(SpringLayout.WEST, targetTitle, 5,
+                             SpringLayout.WEST, this);
         
         //Create target image display label
         targetDisplayLabel = new JLabel();
         targetDisplayLabel.setIcon(new ImageIcon(targetImage));
         targetDisplayLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
-        c.fill = GridBagConstraints.NONE;
-        c.anchor = GridBagConstraints.PAGE_START;
-        c.gridx = 0;
-        c.gridy = 0;
-        c.gridwidth = 1;
-        c.gridheight = 1;
-        c.weightx = 0.5;
-        add(targetDisplayLabel, c);
+        add(targetDisplayLabel);
+        spring.putConstraint(SpringLayout.NORTH, targetDisplayLabel, 5,
+                             SpringLayout.SOUTH, targetTitle);
+        spring.putConstraint(SpringLayout.WEST, targetDisplayLabel, 0,
+                             SpringLayout.WEST, targetTitle);
+        
+        //Create best image title
+        bestTitle = new JLabel();
+        bestTitle.setText("Best");
+        bestTitle.setFont(titleFont);
+        add(bestTitle);
+        spring.putConstraint(SpringLayout.NORTH, bestTitle, 0,
+                             SpringLayout.NORTH, targetTitle);
+        spring.putConstraint(SpringLayout.WEST, bestTitle, 5,
+                             SpringLayout.EAST, targetDisplayLabel);
         
         //Create best individual display label
         bestDisplayLabel = new JLabel();
         bestDisplayLabel.setIcon(new ImageIcon(bestImage.getImage()));
         bestDisplayLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
-        c.fill = GridBagConstraints.NONE;
-        c.anchor = GridBagConstraints.PAGE_START;
-        c.gridx = 1;
-        c.gridy = 0;
-        c.gridwidth = 1;
-        c.gridheight = 1;
-        c.weightx = 0.5;
-        add(bestDisplayLabel, c);
+        add(bestDisplayLabel);
+        spring.putConstraint(SpringLayout.NORTH, bestDisplayLabel, 5,
+                             SpringLayout.SOUTH, bestTitle);
+        spring.putConstraint(SpringLayout.WEST, bestDisplayLabel, 0,
+                             SpringLayout.WEST, bestTitle);
+        
+        //Create evolving image title
+        evolvingTitle = new JLabel();
+        evolvingTitle.setText("Evolving");
+        evolvingTitle.setFont(titleFont);
+        add(evolvingTitle);
+        spring.putConstraint(SpringLayout.NORTH, evolvingTitle, 0,
+                             SpringLayout.NORTH, targetTitle);
+        spring.putConstraint(SpringLayout.WEST, evolvingTitle, 5,
+                             SpringLayout.EAST, bestDisplayLabel);
         
         //create evolving individual display label
         evolvingDisplayLabel = new JLabel();
         evolvingDisplayLabel.setIcon(new ImageIcon(evolvingImage.getImage()));
         evolvingDisplayLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
-        c.fill = GridBagConstraints.NONE;
-        c.anchor = GridBagConstraints.PAGE_START;
-        c.gridx = 2;
-        c.gridy = 0;
-        c.gridwidth = 1;
-        c.gridheight = 1;
-        c.weightx = 0.5;
-        add(evolvingDisplayLabel, c);
+        add(evolvingDisplayLabel);
+        spring.putConstraint(SpringLayout.NORTH, evolvingDisplayLabel, 5,
+                             SpringLayout.SOUTH, evolvingTitle);
+        spring.putConstraint(SpringLayout.WEST, evolvingDisplayLabel, 0,
+                             SpringLayout.WEST, evolvingTitle);
         
-        //create get image button
+        //create stats
+        fitnessLabel = new JLabel();
+        fitnessLabel.setText("Fitness: 0%");
+        fitnessLabel.setFont(statFont);
+        add(fitnessLabel);
+        spring.putConstraint(SpringLayout.WEST, fitnessLabel, 5,
+                             SpringLayout.WEST, this);
+        spring.putConstraint(SpringLayout.NORTH, fitnessLabel, 5,
+                             SpringLayout.SOUTH, targetDisplayLabel);
+        
+        generationLabel = new JLabel();
+        generationLabel.setText("Generations: 0");
+        generationLabel.setFont(statFont);
+        add(generationLabel);
+        spring.putConstraint(SpringLayout.NORTH, generationLabel, 5,
+                             SpringLayout.SOUTH, fitnessLabel);
+        spring.putConstraint(SpringLayout.WEST, generationLabel, 0,
+                             SpringLayout.WEST, fitnessLabel);
+        
+        improvementLabel = new JLabel();
+        improvementLabel.setText("Improvements: 0");
+        improvementLabel.setFont(statFont);
+        add(improvementLabel);
+        spring.putConstraint(SpringLayout.NORTH, improvementLabel, 5,
+                             SpringLayout.SOUTH, generationLabel);
+        spring.putConstraint(SpringLayout.WEST, improvementLabel, 0,
+                             SpringLayout.WEST, generationLabel);
+        
+        //create start/pause button
+        toggleStartButton = new JButton();
+        toggleStartButton.setText("Start");
+        toggleStartButton.setFont(titleFont);
+        toggleStartButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                isStart = !isStart;
+                toggleStartButton.setText((isStart) ? "Pause" : "Resume");
+            }
+        });
+        add(toggleStartButton);
+        spring.putConstraint(SpringLayout.NORTH, toggleStartButton, 5,
+                             SpringLayout.SOUTH, targetDisplayLabel);
+        spring.putConstraint(SpringLayout.WEST, toggleStartButton, 0,
+                             SpringLayout.WEST, bestDisplayLabel);
+
+      //create get image button
         getImageButton = new JButton();
         getImageButton.setText("Select a new image");
+        getImageButton.setFont(titleFont);
         getImageButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -128,33 +201,11 @@ public class EvolutionApplet extends JApplet {
                 }
             }
         });
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.anchor = GridBagConstraints.LAST_LINE_START;
-        c.gridx = 0;
-        c.gridy = 1;
-        c.gridwidth = 1;
-        c.gridheight = 1;
-        c.weightx = 0.5;
-        add(getImageButton, c);
-        
-        //create start/pause button
-        toggleStartButton = new JButton();
-        toggleStartButton.setText("Start");
-        toggleStartButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                isStart = !isStart;
-                toggleStartButton.setText((isStart) ? "Pause" : "Resume");
-            }
-        });
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.anchor = GridBagConstraints.LAST_LINE_START;
-        c.gridx = 1;
-        c.gridy = 1;
-        c.gridwidth = 1;
-        c.gridheight = 1;
-        c.weightx = 0.5;
-        add(toggleStartButton, c);
+        add(getImageButton);
+        spring.putConstraint(SpringLayout.NORTH, getImageButton, 5,
+                             SpringLayout.SOUTH, toggleStartButton);
+        spring.putConstraint(SpringLayout.WEST, getImageButton, 0,
+                             SpringLayout.WEST, toggleStartButton);
         
         //run 4ever
         t = new Thread(new Runnable() {
@@ -176,12 +227,17 @@ public class EvolutionApplet extends JApplet {
     }
     
     public void update() {
+    	generation++;
+    	generationLabel.setText("Generation: " + generation);
         evolvingImage = new CircleIndividual(bestImage, 0.02);
         evolvingDisplayLabel.setIcon(new ImageIcon(evolvingImage.getImage()));
         double thisFitness = evolvingImage.calcFitness(targetImage);
         if(thisFitness > bestFitness) {
+            improvements++;
+            improvementLabel.setText("Improvements:" + improvements);
             bestImage = evolvingImage;
             bestFitness = thisFitness;
+            fitnessLabel.setText("Fitness: " + String.format("%.2f", bestFitness) + "%");
             bestDisplayLabel.setIcon(new ImageIcon(bestImage.getImage()));
         }
     }
